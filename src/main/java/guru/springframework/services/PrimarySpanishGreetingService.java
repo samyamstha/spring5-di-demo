03fd9bbb7f975626c3a307848dd92b2ddd965036
samyamshrestha <samyamsth@gmail.com>
2018-06-03 17:34:48 -0400

    Solved the problem by assigning @Component to the GreetingRepositoryImpl in order to register it as a spring bean. Autowired the greetingRepository property in all the service implementation classes.

--- src/main/java/guru/springframework/services/PrimarySpanishGreetingService.java
+++ src/main/java/guru/springframework/services/PrimarySpanishGreetingService.java
@@ -12,7 +12,12 @@ import org.springframework.stereotype.Service;
 @Primary
 public class PrimarySpanishGreetingService implements GreetingService {
 
+
     private GreetingRepository greetingRepository;
+
+    public PrimarySpanishGreetingService(GreetingRepository greetingRepository) {
+        this.greetingRepository = greetingRepository;
+    }
 
     @Override
     public String sayGreeting() {
